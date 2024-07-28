c5d86028b92a22dc95ac28d519b99d38ddb43f16
Nante1805 <hei.nantenaina.2@gmail.com>
2024-07-28 23:37:10 +0300

    style edition

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, ShowGuesser, EditGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { PostList } from "./posts";
 import { UserList } from "./users";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
+    <Resource name="users" list={UserList} show={ShowGuesser}  />
   </Admin>
 );
