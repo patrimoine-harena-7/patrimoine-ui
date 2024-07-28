b3f73823b865f0d17cc1f909f6c0be1f3fd4e9a1
Nante1805 <hei.nantenaina.2@gmail.com>
2024-07-28 23:26:08 +0300

    champ personnalised

--- src/users.tsx
+++ src/users.tsx
@@ -1,12 +1,6 @@
 import { useMediaQuery, Theme } from "@mui/material";
-import {
-  List,
-  SimpleList,
-  Datagrid,
-  TextField,
-  EmailField,
-  UrlField,
-} from "react-admin";
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from "./MyUrlField";
 
 export const UserList = () => {
   const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
@@ -26,7 +20,7 @@ export const UserList = () => {
           <EmailField source="email" />
 
           <TextField source="phone" />
-          <UrlField source="website" />
+          <MyUrlField source="website" />
           <TextField source="company.name" />
         </Datagrid>
       )}
